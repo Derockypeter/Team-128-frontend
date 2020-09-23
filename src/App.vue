3058b4c5918b259e000af86cff4e0f52eafb8117
Mansur Ibrahim Nok <mansuribrahimnok@gmail.com>
2020-09-23 16:04:18 +0100

    integrating vue

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+
+
+<template>
+<div id="app">
+<Header/>
+</div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue';
+import Header from './components/Header.vue';
+
+export default {
+    name: 'App',
+    components: {
+        //HelloWorld,
+        Header
+    }
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+<header>
+    <div class="container">
+        <div id="branding">
+            <h1><img src="RailT.jpg"></h1>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li class="current"><a href="about-us.html">About Us</a></li>
+                <li><a href="team.html">Our Team</a></li>
+                <li><a href="sign-in.html">login</a></li>
+            </ul>
+                <button>
+                  <p>  <a href="form.html">sign-up</a></p>
+                </button>
+        </nav>
+    </div>
+    
+</header>
+</template>
+
+<script>
+export default {
+    
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  
+}
+}
+</script>
